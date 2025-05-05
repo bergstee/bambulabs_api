56b8a4a4ecd111c350aee865c4b7a31a6980301e
Idan Bush <idan.bushari@gmail.com>
2025-05-05 17:53:59 +0300

    Add more Bambu filament settings

--- bambulabs_api/filament_info.py
+++ bambulabs_api/filament_info.py
@@ -46,6 +46,36 @@ class Filament(AMSFilamentSettings, Enum):
     SUPPORT_G: The Support G filament settings.
     SUPPORT_W: The Support W filament settings.
     BAMBU_TPU_95A: The Bambu TPU 95A filament settings.
+    BAMBU_ASA_AERO: The Bambu ASA AERO filament settings.
+    BAMBU_PLA_METAL: The Bambu PLA Metal filament settings.
+    BAMBU_PETG_TRANSLUCENT: The Bambu PETG Translucent filament settings.
+    BAMBU_PLA_MARBLE: The Bambu PLA Marble filament settings.
+    BAMBU_PLA_WOOD: The Bambu PLA Wood filament settings.
+    BAMBU_PLA_SILK_PLUS: The Bambu PLA Silk Plus filament settings.
+    BAMBU_PETG_HF: The Bambu PETG HF filament settings.
+    BAMBU_TPU_FOR_AMS: The Bambu TPU for AMS filament settings.
+    BAMBU_SUPPORT_FOR_ABS: The Bambu Support for ABS filament settings.
+    BAMBU_PC_FR: The Bambu PC FR filament settings.
+    BAMBU_PLA_GALAXY: The Bambu PLA Galaxy filament settings.
+    BAMBU_PA6_GF: The Bambu PA6 GF filament settings.
+    BAMBU_PLA_AERO: The Bambu PLA AERO filament settings.
+    BAMBU_ASA_CF: The Bambu ASA CF filament settings.
+    BAMBU_PETG_CF: The Bambu PETG CF filament settings.
+    BAMBU_SUPPORT_FOR_PA_PET: The Bambu Support for PA PET filament settings.
+    BAMBU_PLA_SPARKLE: The Bambu PLA Sparkle filament settings.
+    BAMBU_ABS_GF: The Bambu ABS GF filament settings.
+    BAMBU_PAHT_CF: The Bambu PAHT CF filament settings.
+    BAMBU_PLA_BASIC: The Bambu PLA Basic filament settings.
+    BAMBU_PLA_MATTE: The Bambu PLA Matte filament settings.
+    BAMBU_PA6_CF: The Bambu PA6 CF filament settings.
+    BAMBU_PLA_SILK: The Bambu PLA Silk filament settings.
+    BAMBU_PVA: The Bambu PVA filament settings.
+    BAMBU_PLA_CF: The Bambu PLA CF filament settings.
+    BAMBU_SUPPORT_FOR_PLA_PETG: The Bambu Support for PLA PETG filament settings.
+    BAMBU_TPU_95A_HF: The Bambu TPU 95A HF filament settings.
+    BAMBU_PPA_CF: The Bambu PPA CF filament settings.
+    BAMBU_ASA: The Bambu ASA filament settings.
+    BAMBU_PLA_GLOW: The Bambu PLA Glow filament settings.
     ABS: The ABS filament settings.
     ASA: The ASA filament settings.
     PA: The PA filament settings.
@@ -67,6 +97,37 @@ class Filament(AMSFilamentSettings, Enum):
     SUPPORT_G = "GFS01", 190, 250, "PA-S"
     SUPPORT_W = "GFS00", 190, 250, "PLA-S"
     BAMBU_TPU_95A = "GFU01", 200, 250, "TPU"
+
+    BAMBU_ASA_AERO = "GFB02", 240, 280, "ASA"
+    BAMBU_PLA_METAL = "GFA02", 190, 230, "PLA"
+    BAMBU_PETG_TRANSLUCENT = "GFG01", 230, 260, "PETG"
+    BAMBU_PLA_MARBLE = "GFA07", 190, 230, "PLA"
+    BAMBU_PLA_WOOD = "GFA16", 190, 240, "PLA"
+    BAMBU_PLA_SILK_PLUS = "GFA06", 210, 240, "PLA"
+    BAMBU_PETG_HF = "GFG02", 230, 260, "PETG"
+    BAMBU_TPU_FOR_AMS = "GFU02", 230, 230, "TPU"
+    BAMBU_SUPPORT_FOR_ABS = "GFS06", 190, 220, "Support"
+    BAMBU_PC_FR = "GFC01", 260, 280, "PC"
+    BAMBU_PLA_GALAXY = "GFA15", 190, 230, "PLA"
+    BAMBU_PA6_GF = "GFN08", 260, 290, "PA6"
+    BAMBU_PLA_AERO = "GFA11", 220, 260, "PLA"
+    BAMBU_ASA_CF = "GFB51", 250, 280, "ASA"
+    BAMBU_PETG_CF = "GFG50", 240, 270, "PETG"
+    BAMBU_SUPPORT_FOR_PA_PET = "GFS03", 280, 300, "Support"
+    BAMBU_PLA_SPARKLE = "GFA08", 190, 230, "PLA"
+    BAMBU_ABS_GF = "GFB50", 240, 270, "ABS"
+    BAMBU_PAHT_CF = "GFN04", 260, 290, "PAHT"
+    BAMBU_PLA_BASIC = "GFA00", 190, 230, "PLA"
+    BAMBU_PLA_MATTE = "GFA01", 190, 230, "PLA"
+    BAMBU_PA6_CF = "GFN05", 260, 290, "PA6"
+    BAMBU_PLA_SILK = "GFA05", 210, 230, "PLA"
+    BAMBU_PVA = "GFS04", 220, 250, "PVA"
+    BAMBU_PLA_CF = "GFA50", 210, 240, "PLA"
+    BAMBU_SUPPORT_FOR_PLA_PETG = "GFS05", 190, 220, "Support"
+    BAMBU_TPU_95A_HF = "GFU00", 230, 230, "TPU"
+    BAMBU_PPA_CF = "GFN06", 280, 310, "PPA"
+    BAMBU_ASA = "GFB01", 240, 270, "ASA"
+    BAMBU_PLA_GLOW = "GFA12", 190, 230, "PLA"
 
     ABS = "GFB99", 240, 270, "ABS"
     ASA = "GFB98", 240, 270, "ASA"
